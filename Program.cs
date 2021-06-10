2d3344c4e752ca482711bff9ad17e8e7b4d9ff77
Sebastián Gómez Morales <sebastian61910.sg@gmail.com>
2021-06-10 11:47:43 -0600

    Se implementa el desarrollo del programa.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace P8_DiaryEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            CultureInfo culture = new CultureInfo("es-MX");
+            string todayAsStr = today.ToString(culture);
+            string[] fechaHora = todayAsStr.Split(" ");
+            string fechaDeHoy = fechaHora[0];
+            string fechaFinal = fechaDeHoy.Replace("/", "-");
+
+            try
+            {
+                Console.WriteLine("¡Bienvenido a tu diario! Porfavor, introduce la entrada del día de hoy:");
+                string entrada = Console.ReadLine();
+
+                using (StreamWriter sw = new StreamWriter(fechaFinal))
+                {
+                    sw.WriteLine(entrada);
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Error, por favor introduce carácteres válidos");
+            }
+            finally
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
